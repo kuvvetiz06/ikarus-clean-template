e563c50c646763d19faa430f48fea9e84dbe62ef
HuseyiN Gokdemir <huseyingokdemir@msn.com>
2025-07-04 21:18:18 +0300

    Add DeleteTenantCommand, validator and handler to Application layer

---
+++ IKARUSWEB.Application/Commands/DeleteTenant/DeleteTenantCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKARUSWEB.Application.Commands.DeleteTenant
+{
+    // Soft delete için sadece Id ve kullanıcı bilgisi yeterli
+    public record DeleteTenantCommand(Guid Id, string ModifiedUser) : IRequest<Unit>;
+}
---
+++ IKARUSWEB.Application/Commands/DeleteTenant/DeleteTenantCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using IKARUSWEB.Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKARUSWEB.Application.Commands.DeleteTenant
+{
+    public class DeleteTenantCommandHandler : IRequestHandler<DeleteTenantCommand, Unit>
+    {
+        private readonly ITenantRepository _repo;
+
+        public DeleteTenantCommandHandler(ITenantRepository repo) =>
+            _repo = repo;
+
+        public async Task<Unit> Handle(DeleteTenantCommand request, CancellationToken ct)
+        {
+            var tenant = await _repo.GetByIdAsync(request.Id);
+            if (tenant is null)
+                throw new KeyNotFoundException($"Tenant (Id: {request.Id}) bulunamadı.");
+
+            // Soft delete
+            tenant.MarkDeleted();
+            // Silme işlemini yapan kullanıcıyı da kaydet
+            tenant.MarkModified(request.ModifiedUser);
+
+            await _repo.UpdateAsync(tenant);
+            return Unit.Value;
+        }
+    }
+}
